a2bb7eb762a5fc83f33fff76bb1e6654cb28e28a
LeeJaejun98 <jj980302@naver.com>
2023-03-21 16:55:54 +0900

    블로그 목록 뷰 생성

--- CloudProogramming/blog/views.py
+++ CloudProogramming/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+# 정적 렌더링
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+    return render(request, 'blog/index.html',
+                  {'posts': posts})
