e4cbafaeb3b6f9cb907a33bb9f3bb3370672b209
LeeJaejun98 <jj980302@naver.com>
2023-03-21 15:23:36 +0900

    blog/Post 모 델 생 성

--- CloudProogramming/blog/models.py
+++ CloudProogramming/blog/models.py
@@ -4,7 +4,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    create_at = models.DateTimeField()
+    create_at = models.DateTimeField(auto_now_add=True) #record 추가할시 시간 수정
+    updated_at = models.DateTimeField(auto_now=True)    #
 
     def __str__(self):  #java로 따지면 toString()
         return f'[{self.pk}]{self.title}'
