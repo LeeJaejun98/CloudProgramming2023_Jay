7494f87cb7b6b6ec14a6ddc77c60bcaf7950a760
LeeJaejun98 <jj980302@naver.com>
2023-03-28 15:41:15 +0900

    CBV refactoring

--- CloudProogramming/blog/urls.py
+++ CloudProogramming/blog/urls.py
@@ -2,6 +2,7 @@
 from . import views     #. = 현재 파일에서의 views를 쓰겠다
 from django.urls import path, include
 urlpatterns = [
-    path('', views.index),
-    path('<int:post_num>/', views.single_post_page),
+    path('', views.PostList.as_view()),
+    #path('', views.index),
+    path('<int:pk>/', views.PostDetail.as_view()),
 ]
--- CloudProogramming/blog/views.py
+++ CloudProogramming/blog/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 # 정적 렌더링
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(request, 'blog/index.html',
-                  {'posts': posts})
+# def index(request):
+# posts = Post.objects.all().order_by('-pk')
+#  return render(request, 'blog/post_list.html',
+#                 {'posts': posts})
+#   -> class based view 만들기 때문에 필요없음
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'  # 내림차순으로 정렬해주기 위해
 
 
 def single_post_page(request, post_num):
     post = Post.objects.get(pk=post_num)
     return render(request,
-                  'blog/single_post_page.html',
+                  'blog/post_detail.html',
                   {'post': post}
                   )
+
+
+class PostDetail(DetailView):
+    model = Post
