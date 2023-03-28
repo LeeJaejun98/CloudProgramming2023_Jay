dfdf1a7d44e4e8611bed4a52b5364cb14a4a8b7a
LeeJaejun98 <jj980302@naver.com>
2023-03-28 17:05:14 +0900

    static files introduced, image attatchment, file attatchment

--- CloudProogramming/blog/models.py
+++ CloudProogramming/blog/models.py
@@ -4,6 +4,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     create_at = models.DateTimeField(auto_now_add=True) #record 추가할시 시간 수정
     updated_at = models.DateTimeField(auto_now=True)    #
 
