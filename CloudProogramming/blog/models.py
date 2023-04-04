d8ff0db97309bf07ffc842d7008b9a554d3d44a4
LeeJaejun98 <jj980302@naver.com>
2023-04-04 16:16:37 +0900

    카테고리 추가

--- CloudProogramming/blog/models.py
+++ CloudProogramming/blog/models.py
@@ -1,6 +1,19 @@
 import os.path
 
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -8,14 +21,20 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
-    create_at = models.DateTimeField(auto_now_add=True) #record 추가할시 시간 수정
-    updated_at = models.DateTimeField(auto_now=True)    #
+    create_at = models.DateTimeField(auto_now_add=True)  # record 추가할시 시간 수정
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):  #java로 따지면 toString()
-        return f'[{self.pk}]{self.title}'
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    # ondelete -> 만약에 사용자가 삭제되면 어떻게 할것인가?
+    # CASCADE -> 연결 되어 있는 Post 레코드까지 삭제
+
+    def __str__(self):  # java로 따지면 toString()
+        return f'[{self.pk}]{self.title}::{self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
 
     def get_file_name(self):
-        return os.path.basename(self.file_upload.name)      #파일의 이름을 basename만 잘라서 보여주기 위해
+        return os.path.basename(self.file_upload.name)  # 파일의 이름을 basename만 잘라서 보여주기 위해
