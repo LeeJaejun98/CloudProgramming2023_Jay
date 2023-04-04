5f492431b5e658ec13318ec477f62e06cca80ef9
LeeJaejun98 <jj980302@naver.com>
2023-04-04 17:57:48 +0900

    blog 세부 tag, category 추가

--- CloudProogramming/blog/models.py
+++ CloudProogramming/blog/models.py
@@ -4,6 +4,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Tag(models.Model):
+    name=models.CharField(max_length=50)
+    slug=models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -13,6 +23,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
 
 
 class Post(models.Model):
@@ -26,6 +39,7 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     # ondelete -> 만약에 사용자가 삭제되면 어떻게 할것인가?
     # CASCADE -> 연결 되어 있는 Post 레코드까지 삭제
--- CloudProogramming/blog/urls.py
+++ CloudProogramming/blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     #path('', views.index),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.categories_page),
+    path('tag/<str:slug>/', views.tag_page)
 ]
